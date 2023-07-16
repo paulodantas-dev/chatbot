97d1772d9dd747dd024c16535cfa7de701186ac5
Paulo Dantas <paulodantasbjr@gmail.com>
2023-07-16 19:54:44 +0100

    input type

--- script.js
+++ script.js
@@ -1,104 +1,120 @@
-const wrapper = document.querySelector(".wrapper"),
-  inputPart = document.querySelector(".input-part"),
-  infoTxt = inputPart.querySelector(".info-txt"),
-  inputField = inputPart.querySelector("input"),
-  locationBtn = inputPart.querySelector("button"),
-  weatherPart = wrapper.querySelector(".weather-part"),
-  wIcon = weatherPart.querySelector("img"),
-  arrowBack = wrapper.querySelector("header i");
+const wrapper = document.querySelector(".wrapper");
+const msgTxt = document.querySelector(".msg-txt");
+const inputField = document.querySelector("#cityInput");
+const getWeatherBtn = document.querySelector("#getWeatherBtn");
+const weatherPart = document.querySelector(".weather-part");
+const weatherIcon = document.querySelector("#weatherIcon");
+const temperatureElement = document.querySelector("#temperature");
+const weatherDescriptionElement = document.querySelector("#weatherDescription");
+const locationElement = document.querySelector("#location");
+const feelsLikeTemperatureElement = document.querySelector(
+  "#feelsLikeTemperature"
+);
+const humidityElement = document.querySelector("#humidity");
+const arrowBack = document.querySelector("header i");
 
-let api;
+const apiKey = "466c356b7c61ed8c98b8eb8295b662e2"; // Substitua com a sua chave de API do OpenWeatherMap
 
+getWeatherBtn.addEventListener("click", getWeather);
 inputField.addEventListener("keyup", (e) => {
-  // if user pressed enter btn and input value is not empty
-  if (e.key == "Enter" && inputField.value != "") {
-    requestApi(inputField.value);
+  if (e.key === "Enter") {
+    getWeather();
   }
 });
 
-locationBtn.addEventListener("click", () => {
-  if (navigator.geolocation) {
-    // if browser support geolocation api
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  } else {
-    alert("Your browser not support geolocation api");
+function getWeather() {
+  const city = inputField.value.trim();
+  if (city === "") {
+    return getCurrentLocationWeather();
   }
-});
 
-function requestApi(city) {
-  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=466c356b7c61ed8c98b8eb8295b662e2`;
-  fetchData();
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  fetchWeatherData(apiUrl);
 }
 
-function onSuccess(position) {
-  const { latitude, longitude } = position.coords; // getting lat and lon of the user device from coords obj
-  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=466c356b7c61ed8c98b8eb8295b662e2`;
-  fetchData();
-}
+function fetchWeatherData(apiUrl) {
+  msgTxt.textContent = "Getting weather details...";
+  msgTxt.classList.add("pending");
 
-function onError(error) {
-  // if any error occur while getting user location then we'll show it in infoText
-  infoTxt.innerText = error.message;
-  infoTxt.classList.add("error");
-}
-
-function fetchData() {
-  infoTxt.innerText = "Getting weather details...";
-  infoTxt.classList.add("pending");
-  // getting api response and returning it with parsing into js obj and in another
-  // then function calling weatherDetails function with passing api result as an argument
-  fetch(api)
-    .then((res) => res.json())
-    .then((result) => weatherDetails(result))
-    .catch(() => {
-      infoTxt.innerText = "Something went wrong";
-      infoTxt.classList.replace("pending", "error");
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod === 200) {
+        showWeatherData(data);
+      } else {
+        showError(data.message);
+      }
+    })
+    .catch((error) => {
+      console.log("Error fetching weather data:", error);
+      showError("Something went wrong. Please try again.");
     });
 }
 
-function weatherDetails(info) {
-  if (info.cod == "404") {
-    // if user entered city name isn't valid
-    infoTxt.classList.replace("pending", "error");
-    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
-  } else {
-    //getting required properties value from the whole weather information
-    const city = info.name;
-    const country = info.sys.country;
-    const { description, id } = info.weather[0];
-    const { temp, feels_like, humidity } = info.main;
-
-    // using custom weather icon according to the id which api gives to us
-    if (id == 800) {
-      wIcon.src = "icons/clear.svg";
-    } else if (id >= 200 && id <= 232) {
-      wIcon.src = "icons/storm.svg";
-    } else if (id >= 600 && id <= 622) {
-      wIcon.src = "icons/snow.svg";
-    } else if (id >= 701 && id <= 781) {
-      wIcon.src = "icons/haze.svg";
-    } else if (id >= 801 && id <= 804) {
-      wIcon.src = "icons/cloud.svg";
-    } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
-      wIcon.src = "icons/rain.svg";
-    }
-
-    //passing a particular weather info to a particular element
-    weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
-    weatherPart.querySelector(".weather").innerText = description;
-    weatherPart.querySelector(
-      ".location span"
-    ).innerText = `${city}, ${country}`;
-    weatherPart.querySelector(".temp .numb-2").innerText =
-      Math.floor(feels_like);
-    weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
-    infoTxt.classList.remove("pending", "error");
-    infoTxt.innerText = "";
-    inputField.value = "";
-    wrapper.classList.add("active");
+function getWeatherIcon(id) {
+  if (id == 800) {
+    return (weatherIcon.src = "icons/clear.svg");
+  } else if (id >= 200 && id <= 232) {
+    return (weatherIcon.src = "icons/storm.svg");
+  } else if (id >= 600 && id <= 622) {
+    return (weatherIcon.src = "icons/snow.svg");
+  } else if (id >= 701 && id <= 781) {
+    return (weatherIcon.src = "icons/haze.svg");
+  } else if (id >= 801 && id <= 804) {
+    return (weatherIcon.src = "icons/cloud.svg");
+  } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+    return (weatherIcon.src = "icons/rain.svg");
   }
+}
+
+function showWeatherData(data) {
+  const { name, sys, weather, main } = data;
+
+  const icon = getWeatherIcon(weather[0].id);
+
+  weatherIcon.src = icon;
+  temperatureElement.textContent = Math.round(main.temp);
+  weatherDescriptionElement.textContent = weather[0].description;
+  locationElement.textContent = `${name}, ${sys.country}`;
+  feelsLikeTemperatureElement.textContent = Math.round(main.feels_like);
+  humidityElement.textContent = `${main.humidity}%`;
+
+  msgTxt.textContent = "";
+  msgTxt.classList.remove("pending");
+  wrapper.classList.add("active");
+  clearInput();
+}
+
+function showError(message) {
+  msgTxt.textContent = message;
+  msgTxt.classList.add("error");
+}
+
+function clearInput() {
+  inputField.value = "";
 }
 
 arrowBack.addEventListener("click", () => {
   wrapper.classList.remove("active");
 });
+
+function getCurrentLocationWeather() {
+  if (navigator.geolocation) {
+    msgTxt.textContent = "Getting current location...";
+    msgTxt.classList.add("pending");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+        fetchWeatherData(apiUrl);
+      },
+      (error) => {
+        console.log("Error getting current location:", error);
+        showError("Could not get current location. Please try again.");
+      }
+    );
+  } else {
+    showError("Geolocation is not supported by your browser.");
+  }
+}
